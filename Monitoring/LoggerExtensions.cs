0b78bc8c836304f80351b15129942f407f76b5ac
Bjørn Göttler <bjoern@goettler.dk>
2025-05-08 12:39:48 +0200

    Logging: Add Logging

--- Monitoring/LoggerExtensions.cs
+++ Monitoring/LoggerExtensions.cs
@@ -1,6 +1,16 @@
+using System.Runtime.CompilerServices;
+using Serilog;
+
 namespace Monitoring;
 
-public class LoggerExtensions
+public static class LoggerExtensions
 {
-    
+    public static ILogger Here(
+        this ILogger logger,
+        [CallerMemberName] string memberName = "",
+        [CallerFilePath] string sourceFilePath = "",
+        [CallerLineNumber] int sourceLineNumber = 0)
+        => logger.ForContext("MemberName", memberName)
+            .ForContext("SourceFilePath", sourceFilePath)
+            .ForContext("SourceLineNumber", sourceLineNumber);
 }
