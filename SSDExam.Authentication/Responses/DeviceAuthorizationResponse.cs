50b01b26ac90023488c6432fd49f94df3304384e
Bjørn Göttler <bjoern@goettler.dk>
2025-05-13 11:50:18 +0200

    auth: Add Auth0 responses

---
+++ SSDExam.Authentication/Responses/DeviceAuthorizationResponse.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+
+namespace SSDExam.Authentication.Responses;
+
+public class DeviceAuthorizationResponse
+{
+    [JsonPropertyName("device_code")]
+    public string DeviceCode { get; set; }
+    
+    [JsonPropertyName("user_code")]
+    public string UserCode { get; set; }
+    
+    [JsonPropertyName("verification_uri")]
+    public string VerificationUri { get; set; }
+    
+    [JsonPropertyName("verification_uri_complete")]
+    public string VerificationUriComplete { get; set; }
+    
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+    
+    [JsonPropertyName("interval")]
+    public int Interval { get; set; }
+}
---
+++ SSDExam.Authentication/Responses/ErrorResponse.cs
@@ -0,0 +1,12 @@
+using System.Text.Json.Serialization;
+
+namespace SSDExam.Authentication.Responses;
+
+public class ErrorResponse
+{
+    [JsonPropertyName("error")]
+    public string Error { get; set; }
+    
+    [JsonPropertyName("error_description")]
+    public string ErrorDescription { get; set; }
+}
